ad39cbed8241176ca5a2d04ddf6deabd0e053924
rishav2201 <149477682+rishav2201@users.noreply.github.com>
2024-02-27 16:21:41 +0000

    container config

---
+++ test/test.js
@@ -0,0 +1,28 @@
+// console.log("Rishav");
+
+
+// hoisting in java script
+var x=7;
+function getName(){
+    console.log("Namaste Javascript");
+}
+
+getName();
+console.log(x);
+
+function b(){
+    var x = 10;
+    console.log(x);
+}
+
+
+b();
+
+
+const accountId = 1212121
+let accountEmail = "Rai@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
